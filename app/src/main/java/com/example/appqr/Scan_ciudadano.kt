7abefb982afb6fb80c5ba33e6c8d9295e5aa3ec2
rodrigo <rodrigo.27.06.cg@gmail.com>
2023-03-01 10:17:26 -0500

    actualizado crear activity de inicio y vinculacion a activity_scan_inspector.xml renombrandolo

---
+++ app/src/main/java/com/example/appqr/Scan_ciudadano.kt
@@ -0,0 +1,11 @@
+package com.example.appqr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Scan_ciudadano : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_ciudadano)
+    }
+}
